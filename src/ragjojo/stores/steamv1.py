f107998309fa1560e9f8771e12e0e72e89cacd49
Virrann <cunha.vargas.oliveira@gmail.com>
2025-11-07 14:42:04 -0300

    cut class def off

--- src/ragjojo/stores/steamv1.py
+++ src/ragjojo/stores/steamv1.py
@@ -1,30 +1,27 @@
-from pickle import TRUE
 import requests
 import os
 
-class steam:
+__url = f"https://api.steampowered.com/IFamilyGroupsService/GetSharedLibraryApps/v1/"
 
-    __url = f"https://api.steampowered.com/IFamilyGroupsService/GetSharedLibraryApps/v1/"
-
-    def __api_key() -> str:
-        return str(os.getenv("STEAM_WEBAPI_TOKEN"))
-        
-    def __id() -> str:
-        return os.getenv("STEAM_ID")
+def __api_key() -> str:
+    return str(os.getenv("STEAM_WEBAPI_TOKEN"))
     
-    
-    def get_family_games() -> requests.json(): 
+def __id() -> str:
+    return os.getenv("STEAM_ID")
 
-        params = {
-            "access_token": steam.__api_key(), 
-            "family_groupid": 0,
-            "include_own": True,
-            "include_excluded": True,
-            "include_free": True,
-            "steamid": steam.__id()
-        }
 
-        resp = requests.get(steam.__url, params=params)
-        return resp.json()
+def get_family_games(): 
+
+    params = {
+        "access_token": __api_key(), 
+        "family_groupid": 0,
+        "include_own": True,
+        "include_excluded": True,
+        "include_free": True,
+        "steamid": __id()
+    }
+
+    resp = requests.get(__url, params=params)
+    return resp.json()
         
     
